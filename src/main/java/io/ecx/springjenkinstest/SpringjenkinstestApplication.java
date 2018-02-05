688a77a93a92189fc93cfc61645ca556ff1d1a8c
Mario Tudan <mario.tudan@ecx.io>
2018-02-05 08:55:32 +0100

    Created project

---
+++ src/main/java/io/ecx/springjenkinstest/SpringjenkinstestApplication.java
@@ -0,0 +1,12 @@
+package io.ecx.springjenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjenkinstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjenkinstestApplication.class, args);
+	}
+}
